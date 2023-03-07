438e3c5080a361d5da5951e558db429aefcd88d1
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-07 12:40:03 +0400

    Hello  and remove all extra spaces and inicials

---
+++ Hello.py
@@ -0,0 +1,27 @@
+name = input("What is your name?").strip()
+
+print(f"Hello, {name}, Nice to meet you")
+
+
+print("Hello", input("What is your name?").strip())
+
+
+name = input("What is your name? ")
+
+if name:
+    print("Hello,", name)
+
+name_1 = input("What is your name? ")
+if name_1:
+    print("გამარჯობა,", name_1)
+name_2 = input ("What is your name? ")
+if name_2:
+    print("hallo,", name_2)
+name_3 = input ("What is your name? ")
+if name_3:
+    print("bonjurno",  name_3)
+name_4 = input ("What is your name? ")
+if name_3:
+    print("bonjurno",  name_4)
+
+
---
+++ remove_extra_space.py
@@ -0,0 +1,5 @@
+sentence = input("Write a sentence about yourself:")
+
+clear_sentence = " ".join(sentence.split())
+
+print(clear_sentence)
