d72c1f759383965e7463c247af8021304c4e87e1
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-07 17:08:42 +0400

    f

--- Hello_1.py
+++ Hello_1.py
@@ -2,18 +2,19 @@ import random
 
 name = input("What is your name? ")
 
-def random_greeting ():
+def random_greeting():
     greeting = [
         f"Hello,{name}",
         f"gamarjoba, {name}",
         f"salami, {name}",
-        f"hallo", {name}
+        f"hallo, {name}",
 
     ]
 
-    print(greeting[
-        random.randint(0, len(greeting)-1)
-    ])
+    greet = greeting[random.randint(0, len(greeting)-1)]
+
+
+    print(greet)
 
 random_greeting()
 
