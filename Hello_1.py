ff817e0b44252024b549b284dfabe7b7d06406cd
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-07 13:44:01 +0400

    doind with functions

---
+++ Hello_1.py
@@ -0,0 +1,18 @@
+import random
+
+name = input("What is your name? ")
+
+def random_greeting ():
+    greeting = [
+        f"Hello,{name}",
+        f"gamarjoba, {name}",
+        f"salami, {name}",
+        f"hallo", {name}
+
+    ]
+
+    print(greeting[
+        random.randint(0, len(greeting))
+    ])
+
+random_greeting()
